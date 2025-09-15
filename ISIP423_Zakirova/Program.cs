9e440ad776de51758607ce85f61af69fc32d6686
234836 <zakmira2007@gmail.com>
2025-09-15 11:35:44 +0300

    Для начала, создаем отдельную переменную для подсчета кол-ва операций. Далее, создаются два массива - для чисел и самих операций (согласно подсказкам), и здесь стартует ввод самих операций. Указываем счетчик и разделитель для них

--- ISIP423_Zakirova/Program.cs
+++ ISIP423_Zakirova/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int count;
+        while (!int.TryParse(Console.ReadLine(), out count) || count < 2 || count > 40);
+        {
+            Console.WriteLine("Ошибка! Можно вводить от 2 до 40 операций");
+        }
+    
+        string[] oper = new string[count];
+        double[] amount = new double[count];
+
+        Console.WriteLine("Введите операции в формате: Название услуги или товара; Кол-во средств. Необходимы траты в рублях!");
+
+        for (int i = 0; i < count; i++)
+        {
+            while (true)
+            {
+                Console.Write($"{i + 1}. ");
+                string input = Console.ReadLine();
+                string[] parts = input.Split(';');
+
+                if (parts.Length == 2 )
+                {
+                    string name = parts[0].Trim();
+                    string value = parts[1].Trim();
+                }
+            }
+            
+
+        }
+
+
+
+    }
+}
