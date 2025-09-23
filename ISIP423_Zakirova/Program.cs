472f508b9a81b5a749aeff0094a7baaee18ade04
Elmira <zakmira2007@gmail.com>
2025-09-23 18:01:13 +0300

    создается класс с переменными и словарем для частоты слов

--- ISIP423_Zakirova/Program.cs
+++ ISIP423_Zakirova/Program.cs
@@ -1,3 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("uzbbb");
-Console.WriteLine("ffff");
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class TextStatistics
+{
+    public string Text { get; set; }
+    public int WordCount { get; set; }
+    public string ShortestWord { get; set; }
+    public int SentenceCount { get; set; }
+    public int ABcount { get; set; } //гласные и согласные
+    public string LongestWord { get; set; }
+    public Dictionary<char, int> LetterFreq { get; set; } = new Dictionary<char, int>(); //почитать про char!
+
+    public 
+
+}
