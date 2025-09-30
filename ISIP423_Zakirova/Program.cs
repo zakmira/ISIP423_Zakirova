2c9a21737054e4dc2f11f303bebe50c397274949
Elmira <zakmira2007@gmail.com>
2025-09-30 11:29:47 +0300

    входные данные

--- ISIP423_Zakirova/Program.cs
+++ ISIP423_Zakirova/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LibraryManagement
+{
+
+    public class Book
+    {
+        private static int _nextId = 1;
+
+        // Доступные жанры
+        public static readonly string[] Genres = {
+            "Художественная литература",
+            "Научная фантастика",
+            "Детектив",
+            "Фэнтези",
+            "Романтика",
+            "Научная литература",
+            "Биография",
+            "История",
+            "Философия",
+            "Поэзия"
+        };
+
+        public int Id { get; private set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public int Year { get; set; }
+        public decimal Price { get; set; }
+
+        public Book(string title, string author, string genre, int year, decimal price)
+        {
+            Id = _nextId++;
+            Title = title;
+            Author = author;
+            Genre = genre;
+            Year = year;
+            Price = price;
+        }
+    }
+}
