a0a5b7597b417d8502c46999466f5c3801de46c4
Elmira <zakmira2007@gmail.com>
2025-10-07 11:05:13 +0300

    абстрактный класс для всех персон

--- ISIP423_Zakirova/Program.cs
+++ ISIP423_Zakirova/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public abstract class Person
+{
+    public int Id { get; }
+    public string Name { get; }
+    public int Age { get; }
+    public string ContactInfo { get; }
+
+    protected Person(int id, string name, int age, string contactInfo)
+    {
+        Id = id;
+        Name = name;
+        Age = age;
+        ContactInfo = contactInfo;
+    }
+
+    public abstract void DisplayInfo();
+}
+
