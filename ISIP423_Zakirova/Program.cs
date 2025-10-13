f1999b2427154d4ec94cd5f2ab8d3862fa020203
Elmira <zakmira2007@gmail.com>
2025-10-13 10:24:37 +0300

    Предметы

--- ISIP423_Zakirova/Program.cs
+++ ISIP423_Zakirova/Program.cs
@@ -205,4 +205,37 @@ namespace TextRPG
         }
     }
 
+    // Предметы
+    public class Weapon
+    {
+        public string Name { get; set; }
+        public int Damage { get; set; }
+
+        public Weapon(string name, int damage)
+        {
+            Name = name;
+            Damage = damage;
+        }
+    }
+
+    public class Armor
+    {
+        public string Name { get; set; }
+        public int Defense { get; set; }
+
+        public Armor(string name, int defense)
+        {
+            Name = name;
+            Defense = defense;
+        }
+    }
+
+    public class HealthPotion
+    {
+        public void Use(Player player)
+        {
+            player.Heal();
+        }
+    }
+
 }
