0df4d7e485cd21c582580dd811b788f1e36efea1
Elmira <zakmira2007@gmail.com>
2025-10-13 10:00:18 +0300

    стартовые данные: персонажи и свойства

--- ISIP423_Zakirova/Program.cs
+++ ISIP423_Zakirova/Program.cs
@@ -1,2 +1,86 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace TextRPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            game.Start();
+        }
+    }
+
+    // Типы врагов
+    public enum EnemyType
+    {
+        Goblin,
+        Skeleton,
+        Mage
+    }
+
+    // Типы боссов
+    public enum BossType
+    {
+        VVG,
+        Kovalsky,
+        ArchmageCPP,
+        PestovCS
+    }
+
+    // Игрок
+    public class Player
+    {
+        public int MaxHP { get; set; } = 100;
+        public int CurrentHP { get; set; } = 100;
+        public Weapon EquippedWeapon { get; set; }
+        public Armor EquippedArmor { get; set; }
+        public bool IsFrozen { get; set; } = false;
+
+        public Player()
+        {
+            // Стартовая экипировка
+            EquippedWeapon = new Weapon("Ржавый меч", 10);
+            EquippedArmor = new Armor("Потертые доспехи", 5);
+        }
+
+        public void TakeDamage(int damage)
+        {
+            CurrentHP -= damage;
+            if (CurrentHP < 0) CurrentHP = 0;
+        }
+
+        public void Heal()
+        {
+            CurrentHP = MaxHP;
+        }
+
+        public bool IsAlive()
+        {
+            return CurrentHP > 0;
+        }
+    }
+
+    public abstract class Enemy
+    {
+        public string Name { get; set; }
+        public int HP { get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; } // Защита
+
+        public abstract int CalculateDamage(Player player, Random random);
+        public abstract void ApplySpecialEffect(Player player, Random random);
+
+        public bool IsAlive()
+        {
+            return HP > 0;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            HP -= damage;
+            if (HP < 0) HP = 0;
+        }
+    }
+}
