9727448d7bb3727b5fc074f0b4f55c42f6ab6323
Elmira <zakmira2007@gmail.com>
2025-11-15 12:33:48 +0300

    архитектура класса пользователя

---
+++ prakktika8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prakktika8
+{
+    internal class Program
+    {
+        public class User
+        {
+            public int Id { get; set; }
+            public string Email { get; set; }
+            public string Password { get; set; }
+
+            public void SignUp() // регистрация
+            {
+
+            }
+
+            public void SignIn() // вход в аккаунт
+            {
+
+            }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
