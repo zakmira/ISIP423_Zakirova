dc2a3ea387acea76e4e57fe9ab0d5ac14a83fc35
Elmira <zakmira2007@gmail.com>
2025-11-15 16:00:19 +0300

    архитектура класса с марекетплейсом + добавлен Core

--- prakktika8/Program.cs
+++ prakktika8/Program.cs
@@ -10,22 +10,130 @@ namespace prakktika8
     {
         public class User
         {
+            public User user {  get; set; }
             public int Id { get; set; }
             public string Email { get; set; }
             public string Password { get; set; }
 
-            public void SignUp() // регистрация
+            public bool SignUp(string email, string password) // регистрация
             {
+                return true;
+            }
+
+            public bool SignIn(string email, string password) // вход в аккаунт
+            {
+                return true;
+            }
+
+            public void HandleSignUp()
+            {
+                Console.WriteLine("Введите email: ");
+                var email = Console.ReadLine();
+                Console.WriteLine("Введите пароль: ");
+                var password = Console.ReadLine();
+                Console.WriteLine("Повторите пароль: ");
+                var passwordRepeat = Console.ReadLine();
+                if (password != passwordRepeat)
+                {
+                    Console.WriteLine("Пароли не совпадают!");
+                }
+
+                if (user.SignUp(email, password))
+                {
+                    Console.WriteLine("Вы зарегистрированы!");
+                }
+                else
+                {
+                    Console.WriteLine("Пользователь уже существует");
+                }
+               
 
             }
 
-            public void SignIn() // вход в аккаунт
+            public void HandleSignIn()
             {
+                Console.WriteLine("Введите email: ");
+                var email = Console.ReadLine();
+                Console.WriteLine("Введите пароль: ");
+                var password = Console.ReadLine();
 
+                if (user.SignIn(email, password))
+                {
+                    Console.WriteLine("Добро пожаловать!");
+                }
+                else
+                {
+                    Console.WriteLine("Неверный email или пароль!");
+                }
             }
         }
+       
+        public class Marketplace
+        {
+            private List<Items> allItems;
+            private List<PVZ> allPVZ;
+            private User currentUser = new User();
+
+            public Marketplace()
+            {
+                allItems = new List<Items>();
+                allPVZ = new List<PVZ>();
+
+                LoadDB();
+
+            }
+
+            public void LoadDB()
+            {
+                allItems = Core.Context.Items.ToList();
+                allPVZ = Core.Context.PVZ.ToList();
+            }
+
+            public void LookItems()
+            {
+                // для незареганных тоже
+            }
+
+            public void AddToCart()
+            {
+
+            }
+
+            public void LookCart()
+            {
+
+            }
+
+            public void DeleteItem()
+            {
+
+            }
+
+            public void Order()
+            {
+                // реализовать выбор: заказать всю корзину или только один предмет
+            }
+            
+            public void ChoosePVZ()
+            {
+                foreach (var pvz in allPVZ)
+                {
+
+                }
+
+            }
+
+            public void LookOrders()
+            {
+                // сортировка по дате
+            }
+        }
+
+
         static void Main(string[] args)
         {
         }
     }
 }
+
+
